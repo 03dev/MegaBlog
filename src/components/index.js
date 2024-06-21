9041e8d0e8d40b61311959bcefc51e517d6e49ff
Dev <102422950+03dev@users.noreply.github.com>
2024-06-22 00:06:04 +0530

    Some more components are added and Authentication layout is designed

--- src/components/index.js
+++ src/components/index.js
@@ -2,12 +2,20 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container"
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button"
+import Input from "./Input"
 import Logo from "./Logo"
+import PostCard from "./PostCard"
+import Select from "./Select"
 
 export {
     Header,
     Footer,
     Container,
     LogoutBtn,
-    Logo
+    Button,
+    Logo,
+    Input,
+    PostCard,
+    Select
 }
